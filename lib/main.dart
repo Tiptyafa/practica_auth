b192f1a2845a2f1eb283bf02ee9244dd9e17a896
Tiptyafa <captain.alatriste15@gmail.com>
2022-01-11 22:43:13 +0300

    Добавлены изображения, такие как логотип и фон для входной страницы

--- lib/main.dart
+++ lib/main.dart
@@ -23,17 +23,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/pexels1.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
           width: double.infinity,
-          //padding: EdgeInsets.symmetric(horizontal: 0),
-          child: Column(children: [
-            SizedBox(height: 35,),
-            SizedBox(width: 110, height: 84,child: Placeholder(),),
-            SizedBox(height: 20,),
-            Text('Введите логин в виде 10 цифр номера телефона',
-            style: TextStyle(fontSize: 15, color: Color.fromRGBO(0, 0, 0, 0.6)),),
-            SizedBox(height: 15,),
-            const SizedBox(width: 224,
-              child: TextField(
+          height: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal : 50),
+          child: SingleChildScrollView(
+            child: Column(children: [
+              SizedBox(height: 100,),
+              SizedBox(width: 110, height: 84,child: Image(image: AssetImage('assets/images1.jpg')),),
+              SizedBox(height: 40,),
+              Text('Введите номер телефона и пароль',
+              style: TextStyle(fontSize: 15, color: Color.fromRGBO(0, 0, 0, 1)),),
+              SizedBox(height: 30,),
+              TextField(
+                keyboardType: TextInputType.phone,
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Color (0xFFeceff1),
@@ -42,10 +50,8 @@ class MyApp extends StatelessWidget {
                   labelText: 'Телефон',
                 ),
               ),
-            ),
-            SizedBox(height: 15,),
-            const SizedBox(width: 224,
-              child: TextField(
+              SizedBox(height: 15,),
+              TextField(
                 obscureText: true,
                 decoration: InputDecoration(
                   filled: true,
@@ -55,26 +61,26 @@ class MyApp extends StatelessWidget {
                   labelText: 'Пароль',
                ),
               ),
-          ),
-            SizedBox(height: 20,),
-            SizedBox(width: 154, height: 42,child:
-             ElevatedButton(onPressed: () {},
-              child: Text('Войти'),
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFF0079D0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(36.0),
+              SizedBox(height: 20,),
+              SizedBox(width: 154, height: 42,child:
+               ElevatedButton(onPressed: () {},
+                child: Text('Войти'),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF0079D0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(36.0),
+                  ),
                 ),
+               ),
               ),
-             ),
-            ),
-            SizedBox(height: 30,),
-            InkWell(child: const Text('Регистрация', style: linkTextStyle),
-                onTap: () {}),
-            SizedBox(height: 10,),
-            InkWell(child: Text('Забыли пароль?', style: linkTextStyle),
-                onTap: () {}),
+              SizedBox(height: 60,),
+              InkWell(child: const Text('Регистрация', style: linkTextStyle),
+                  onTap: () {}),
+              SizedBox(height: 10,),
+              InkWell(child: Text('Забыли пароль?', style: linkTextStyle),
+                  onTap: () {}),
       ],),
+          ),
       ),
       ),
     );
