9d2c1ba01db34855cebe951832060ceb74786001
Tiptyafa <captain.alatriste15@gmail.com>
2022-02-02 00:41:08 +0300

    Отображение данных в виде списка (минимальная версия)

---
+++ lib/Screen_page/ListViewDemo.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ListViewScreen extends StatelessWidget {
+  const ListViewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: MyStatefulWidget()
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text('Человек $index'),
+          selected: index == _selectedIndex,
+          onTap: () {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        );
+      },
+    );
+  }
+}
